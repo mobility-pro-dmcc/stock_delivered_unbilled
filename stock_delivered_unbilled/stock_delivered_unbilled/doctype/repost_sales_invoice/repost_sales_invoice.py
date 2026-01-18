62c438b3a39dcd4f65d1fff81c4be08bc6a137a3
Ahmed Zaytoon <citybirdman@gmail.com>
2026-01-18 14:33:33 +0000

    Fix: duplication in repost sales invoice Feat: add doctype Repost Sales Invoice to the log settings

--- stock_delivered_unbilled/stock_delivered_unbilled/doctype/repost_sales_invoice/repost_sales_invoice.py
+++ stock_delivered_unbilled/stock_delivered_unbilled/doctype/repost_sales_invoice/repost_sales_invoice.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2024, Aerele Technologies Private Limited and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class RepostSalesInvoice(Document):
-	pass
+	@staticmethod
+	def clear_old_logs(days=30):
+		from frappe.query_builder import Interval
+		from frappe.query_builder.functions import Now
+
+		table = frappe.qb.DocType("Repost Sales Invoice")
+		frappe.db.delete(table, filters=(table.modified < (Now() - Interval(days=days))))
